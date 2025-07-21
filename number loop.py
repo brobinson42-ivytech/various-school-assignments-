ffca12fd43dbdf4fa29b4bf6f0371b0e008f13f9
brobinson42-ivytech <brobinson42@ivytech.edu>
2025-07-21 18:19:59 -0400

    Add files via upload

---
+++ number loop.py
@@ -0,0 +1,10 @@
+numbers = 16
+for number in range(1, numbers):
+    str(number)
+    if (number % 2 == 0):
+        print(number, 'is even')
+    else:
+        print(number, 'is odd')
+
+#https://github.com/brobinson42-ivytech/various-school-assignments-
+        #this is my github repository
